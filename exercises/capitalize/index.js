cd3ed3974f68dc6d251a543a6d164d87b9c0cb02
Cristian Rodriguez <cristian_rocas@hotmail.com>
2018-03-07 17:37:16 -0800

    Came up with solution to capitalize algorithm. First solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,27 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const stringArray = str.split('');
+  let capitalize = true;
+  let upperCaseString = '';
+  for (let char of stringArray) {
+    if (capitalize) {
+      upperCaseString += char.toUpperCase();
+      capitalize = false;
+    } else if (char === ' ') {
+      capitalize = true;
+      upperCaseString += char;
+    } else {
+      upperCaseString += char;
+    }
+  }
+  return upperCaseString;
+}
+
+console.log(capitalize('a short sentence'));
 
 module.exports = capitalize;
+
+
+// If a character is a space, then capitalize needs to be set to true. This way when you go over the next character you can use toUpperCase() and push to the string. If a character is capitalize then capitalize should be set to false.
