a23b38d4760772a7ecf45b5d499326e64df9ee30
Cristian Rodriguez <cristian_rocas@hotmail.com>
2018-03-24 08:16:01 -0700

    Solution attempt and also added Stephens first attempt

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,38 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+// ---------------------------------------------------------------------
+// -------------------------- ATTEMPTED SOLUTION  1 -------------------------------
+// function steps(n) {
+//   let stepsLine = '';
+//   for (let i = 0; i < n; i++) {
+//     for (let j = 0; j < n; j++) {
+//       if (j <= i) {
+//         stepsLine += "#";
+//       } else {
+//         stepsLine += ' ';
+//       }
+//     }
+//     stepsLine = '';
+//   }
+// }
+
+// ---------------------------------------------------------------------
+// -------------------------- ATTEMPTED SOLUTION  1 ----------------------
+function steps(n) {
+  for (let row = 0; row < n; row++) {
+    let stair = '';
+    for (let column = 0; column < n; column++) {
+      if (column <= row) {
+        stair += '#';
+      } else {
+        stair += ' ';
+      }
+    }
+    console.log(stair);
+  }
+}
+
+console.log(steps(2));
 
 module.exports = steps;
